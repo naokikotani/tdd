df17746d46a9f197f27b2e769069c97f5226d7b5
naokikotani <nok.naoki.tigers.415@icloud.com>
2025-10-23 10:25:28 +0900

    degenerate object

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -1,9 +1,11 @@
 class Dollar
+  attr_reader :amount
+
   def initialize(amount)
     @amount = amount
   end
 
   def times(multiplier)
-    @amount * multiplier
+    Dollar.new(@amount * multiplier)
   end
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,11 +1,15 @@
 require 'rspec'
+require_relative '../lib/dollar'
 
 RSpec.describe Dollar do
   describe '#times' do
     it do
       five = Dollar.new(5)
       result = five.times(2)
-      expect(result).to eq(10)
+      expect(result.amount).to eq(10)
+
+      result = five.times(3);
+      expect(result.amount).to eq(15)
     end
   end
 end
