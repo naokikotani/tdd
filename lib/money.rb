6ca0207cc953498e16d9050085cf644837d4fe43
naokikotani <nok.naoki.tigers.415@icloud.com>
2025-10-28 08:44:40 +0900

    第8章-実装を隠す

--- lib/money.rb
+++ lib/money.rb
@@ -5,6 +5,20 @@ class Money
     amount == object.amount && self.class == object.class
   end
 
+  def times(multiplier)
+    raise NotImplementedError
+  end
+
+  class << self
+    def dollar(amount)
+      Dollar.new(amount)
+    end
+
+    def franc(amount)
+      Franc.new(amount)
+    end
+  end
+
   protected
 
   attr_reader :amount
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,33 +3,31 @@ require 'rspec'
 RSpec.describe Dollar do
   describe '#times' do
     it do
-      five = Dollar.new(5)
+      five = Money.dollar(5)
 
-      expect(five.times(2)).to eq(Dollar.new(10))
-      expect(five.times(3)).to eq(Dollar.new(15))
+      expect(five.times(2)).to eq(Money.dollar(10))
+      expect(five.times(3)).to eq(Money.dollar(15))
+
+      five = Money.franc(5)
     end
   end
 
   describe '#==' do
     it do
-      five_a = Dollar.new(5)
-      five_b = Dollar.new(5)
-      six = Dollar.new(6)
-
-      expect(Dollar.new(5)).to eq(Dollar.new(5))
-      expect(Dollar.new(6)).to eq(Dollar.new(6))
-      expect(Franc.new(5)).to eq(Franc.new(5))
-      expect(Franc.new(6)).to eq(Franc.new(6))
-      expect(Franc.new(5)).not_to eq(Dollar.new(5))
+      expect(Money.dollar(5)).to eq(Money.dollar(5))
+      expect(Money.dollar(6)).to eq(Money.dollar(6))
+      expect(Money.franc(5)).to eq(Money.franc(5))
+      expect(Money.franc(6)).to eq(Money.franc(6))
+      expect(Money.franc(5)).not_to eq(Money.dollar(5))
     end
   end
 
   describe '#Franctimes' do
     it do
-      five = Franc.new(5)
+      five = Money.franc(5)
 
-      expect(five.times(2)).to eq(Franc.new(10))
-      expect(five.times(3)).to eq(Franc.new(15))
+      expect(five.times(2)).to eq(Money.franc(10))
+      expect(five.times(3)).to eq(Money.franc(15))
     end
   end
 end
